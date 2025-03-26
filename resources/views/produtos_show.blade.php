84b92fd0df3f3b054391e0db3b7b8f50b15762dd
Aluno IFSC <ifsc@CDR-756093.cacador.ifsc.edu.br>
2025-03-26 20:56:08 -0300

    atualização com pagina template

--- resources/views/produtos_show.blade.php
+++ resources/views/produtos_show.blade.php
@@ -1,17 +1,7 @@
-<!doctype html>
-<html>
-    <head>
-'        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap demo</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">'
-    </head>
-    
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-2"></div>
-                <div class="col-sm-8"></div>
+@extends('template')
+
+@section('conteudo')
+
                     <h1>Produtos</h1>
                     <table class="table">
                         <thead>    
@@ -33,17 +23,7 @@
                     </table>
                  <div class="row">
                     <a href="{{ route('produtos.novo') }}"class="btn btn-primary">Novo</a>
-                 </div>
-                <div class="col-sm-2"></div>
-                
-                </div>
-            </div>
-        </div>
+                    
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
-    
-    
-    
-
-<html>
+@endsection                  
+               
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -12,7 +12,7 @@
             <div class="row">
                 <div class="col-sm-2"></div>
                 <div class="col-sm-8"></div>
-                    
+                    @yield('conteudo')
                  </div>
                 <div class="col-sm-2"></div>
                 
